1b6607a9512f808193bbee6e7c8b242983b3082d
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-27 13:14:56 +0300

    Add project files.

---
+++ Lab1ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex3.Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+            tastaura*/
+
+            int numar = 2345;
+            int ultimaCifra = Math.Abs(numar) % 10 ;
+            Console.WriteLine("Ultima cifra a numarului " + numar + " este "  +ultimaCifra);
+        }
+    }
+}
